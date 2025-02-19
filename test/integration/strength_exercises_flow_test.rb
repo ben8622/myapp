2ee69cd04b9bd9a0ee0bd48679bb3fe8d06e2fed
ben8622 <benjaminjknight@gmail.com>
2025-02-19 07:26:02 -0600

    integration test for strength exercise added

--- test/integration/strength_exercises_flow_test.rb
+++ test/integration/strength_exercises_flow_test.rb
@@ -1,15 +1,36 @@
 require "test_helper"
 
 class StrengthExercisesFlowTest < ActionDispatch::IntegrationTest
-  test "can see the welcome page" do
+  test "Can view and add exercise" do
+    # Go to login page
     get "/session/new"
     assert_response 200, status
 
+    # Login
     post "/session", params: { email_address: users(:one).email_address, password: "password" }
-    puts("POST /session status: #{status}")
-    follow_redirect!
+    assert_response 302, status
 
+    # Redirected to home page
+    follow_redirect!
     assert_response 200, status
     assert_equal "/", path
+
+    # Go to str exercises page
+    get "/strength_exercises?"
+    assert_response 200, status
+    assert_equal "/strength_exercises", path
+    assert_dom "td", "Bench"
+
+    # Fill out form and reload page
+    post "/strength_exercises", params: { name: "Test", sets: 3, reps: 8, weight: 135 }
+    assert_response 302, status
+
+    # Reload page
+    follow_redirect!
+    assert_response 200, status
+    assert_equal "/strength_exercises", path
+
+    # Exercise is now displayed on page
+    assert_dom "td", "Test"
   end
 end
