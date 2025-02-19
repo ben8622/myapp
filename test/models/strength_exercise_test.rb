8e438c6dee8b58e13e51c02c5433c8a70b71d76d
ben8622 <benjaminjknight@gmail.com>
2025-02-19 07:31:01 -0600

    validation tests

--- test/models/strength_exercise_test.rb
+++ test/models/strength_exercise_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class StrengthExerciseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "test validations" do
+    invalid_article = StrengthExercise.new
+
+    assert_not invalid_article.save
+
+    valid_exercise = StrengthExercise.new
+    valid_exercise.name = "test"
+    valid_exercise.sets = 3
+    valid_exercise.reps = 8
+    valid_exercise.weight = 135
+    valid_exercise.user_id = users(:one).id
+
+    assert valid_exercise.save
+  end
 end
